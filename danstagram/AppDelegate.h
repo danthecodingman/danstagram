15d40dce7bef5a1141211af21895e6f90405f055
Daniel Avila <danielavila@fb.com>
2019-07-08 15:30:46 -0700

    Initial Commit

---
+++ danstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  danstagram
+//
+//  Created by danielavila on 7/8/19.
+//  Copyright © 2019 danielavila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ danstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  danstagram
+//
+//  Created by danielavila on 7/8/19.
+//  Copyright © 2019 danielavila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
